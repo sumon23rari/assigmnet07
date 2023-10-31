3d7f9f553fe8e420563c01a00660d7bcb72115ce
sumon23rari <sumon784969@gmail.com>
2023-11-01 00:02:24 +0600

    header-part

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+      <header>
+      <h3 className="text-center capitalize font-semibold text-lg text-[#1C1B1B] py-7">Course Registration</h3>
+      </header>
+    );
+};
+
+export default Header;
